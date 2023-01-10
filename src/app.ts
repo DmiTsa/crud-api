11bd708938fd3337af3c1be3af76b480e47cdac7
DmiTsa <you@example.com>
2023-01-10 22:20:15 +0300

    2

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-// request {http.IncomingMessage}
-// response {http.ServerResponse}
 import { createServer, IncomingMessage, ServerResponse } from 'node:http';
 
 const PORT = 4000;
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  res.end('run');
+  if (req.method === 'GET') {
+    if (req.url === '/api/users') {
+      res.statusCode = 200;
+      res.end('ok response');
+    }
+    // console.log(req.url);
+  }
 });
 
 server.listen(PORT, () => {
   console.log('server run');
 });
+
+// const server = createServer(
+//   (request: IncomingMessage, response: ServerResponse) => {
+//     switch (request.method) {
+//       case 'GET':
+//         getController(request, response);
+//         break;
+//       case 'POST':
+//         postController(request, response);
+//         break;
+//       default:
+//         response.statusCode = 500;
+//         response.write(
+//           `Error 500: request type ${request.method} is not supported`
+//         );
+//         response.end();
+//         break;
+//     }
+//   }
+// );
