d48c2ecf9c27b7c52446851a89d545deb32c426f
DmiTsa <you@example.com>
2023-01-10 22:04:34 +0300

    1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('hello');
+// request {http.IncomingMessage}
+// response {http.ServerResponse}
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+
+const PORT = 4000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.end('run');
+});
+
+server.listen(PORT, () => {
+  console.log('server run');
+});
