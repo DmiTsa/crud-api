0200b88a62eeab7db7b4f2d7fd12127c0fd60766
DmiTsa <you@example.com>
2023-01-11 21:12:17 +0300

    h1

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,42 @@
 import { createServer, IncomingMessage, ServerResponse } from 'node:http';
 
 const PORT = 4000;
+const users = [{ id: '001', username: 'user11', age: 30, hobbies: ['hobby1'] }];
+const baseUsersEndpoint = '/api/users';
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  if (req.method === 'GET') {
-    if (req.url === '/api/users') {
-      res.statusCode = 200;
-      res.end('ok response');
-    }
-    // console.log(req.url);
+  switch (req.method) {
+    //create getUsersController postUserController
+    case 'GET':
+      getUsersController(req, res, baseUsersEndpoint);
+      break;
+    default:
+      res.statusCode = 500;
+      res.write(`Error 500: request type ${req.method} is not supported`);
+      res.end();
+      break;
   }
 });
 
 server.listen(PORT, () => {
-  console.log('server run');
+  console.log(`Server is running at port ${PORT}`);
 });
+
+//functions
+function getUsersController(
+  req: IncomingMessage,
+  res: ServerResponse,
+  baseEndpoint: string
+) {
+  if (req.url === baseEndpoint) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(users));
+    return;
+  }
+  const userId = req.url?.slice(baseEndpoint.length + 1);
+  // console.log(userId);
+}
 
 // const server = createServer(
 //   (request: IncomingMessage, response: ServerResponse) => {
@@ -35,3 +57,22 @@ server.listen(PORT, () => {
 //     }
 //   }
 // );
+
+// import { IncomingMessage, ServerResponse } from 'http';
+// import users from '../data/users';
+
+// const getController = (req: IncomingMessage, res: ServerResponse) => {
+//   switch (req.url) {
+//     case `/api/users`:
+//       res.statusCode = 200;
+//       res.setHeader('Content-Type', 'application/json');
+//       res.end(JSON.stringify(users));
+//       break;
+//     default:
+//       res.statusCode = 404;
+//       res.write(`Error 404: Path: ${req.url} is not found`);
+//       res.end();
+//       break;
+//   }
+// };
+// export default getController;
